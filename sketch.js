7dc4b6a2630ec6acf0e6e76916e3036e146b0e32
akritah <sahusunayana1008@gmail.com>
2020-08-27 20:44:02 +0530

    added a new pig

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    pig4=new Pig(200,200,50,30);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -61,4 +62,5 @@ function draw(){
 
     bird.display();
     platform.display();
+    pig4.display();
 }
